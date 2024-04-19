7522141b78b3155bf2c39afd230c2f7607612ad6
Dusk <asutoshpradhan424@gmail.com>
2024-04-19 05:41:45 +0530

    Added more Structure Added Functionality:     - transition on buttons     - logic into new Structure     - display of choice     - display of result     - display of score

--- game.js
+++ game.js
@@ -2,66 +2,60 @@ let choices = ["rock", "paper", "scissors"];
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
-function getPlayerChoice(){
-    return prompt("Rock, Paper, Scissors?").toLowerCase();
-}
-let points = {
+let points={
     player: 0,
     computer: 0
 }
+const curr = document.querySelector(".current");
+const scor = document.querySelector(".points")
+let html;
+let score;
+function getPlayerChoice(e){
+    let pC = e.getAttribute("id");
+    let cC = getComputerChoice();
+    
+    html = pC + " : " + cC + "<br>";
+
+    playRound(pC, cC);
+    score = points.player + " : " + points.computer;
+    curr.innerHTML = html;
+    scor.innerHTML = score;
+}
+
+
+
 function playRound(playerChoice, computerChoice){
-    console.log(playerChoice + " : " + computerChoice);
     if(playerChoice === computerChoice){
-        console.log("It's a tie!");
+        html += "It's a tie!";
     }
     else if (playerChoice === "rock"){
         if(computerChoice === "paper"){
-            console.log("Paper wins");
+            html += "Paper wins";
             points.computer++;
         }
         else{
-            console.log("Rock wins");
+            html += "Rock wins";
             points.player++;
         }
     }
     else if(computerChoice === "rock"){
         if(playerChoice === "paper"){
-            console.log("Paper wins");
+            html += "Paper wins";
             points.player++;
         }
         else{
-            console.log("Rock wins");
+            html += "Rock wins";
             points.computer++;
         }
     }
     else{
         if(playerChoice === "scissors"){
-            console.log("Scissors win");
+            html += "Scissors win";
             points.player++;
         }
         else {
-            console.log("Scissors wins");
+            html += "Scissors wins";
             points.computer++;
         }
     }
 }
-
-function playGame(){
-    for(i = 0;i<5; i++){
-        let pl = getPlayerChoice();
-        let com = getComputerChoice();
-        playRound(pl,com);
-    }
-    if(points.player > points.computer){
-        console.log("You win!");
-    }
-    else if(points.player < points.computer){
-        console.log("You lose!");
-    }
-    else {
-        console.log("Its a Tie!!")
-    }
-}
-
-
-playGame();
